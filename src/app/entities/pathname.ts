c4e29d5b03f8ddbdcea5c76a4a9d4b046b8719eb
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2025-06-02 10:40:19 -0300

    feat: created pathname entity

---
+++ src/app/entities/pathname.ts
@@ -0,0 +1,69 @@
+import { FormatDateAdapter } from "../../infra/adapters/formatDateAdapter";
+import { IdAdapter } from "../../infra/adapters/idAdapter";
+
+type ConstructorProps = {
+  id: string;
+  value: string;
+  domainId: string;
+  trafficSourceId: string;
+  createdAt: Date;
+};
+
+type CreatePathnameProps = {
+  value: string;
+  domainId: string;
+  trafficSourceId: string;
+};
+
+type RestorePathnameProps = ConstructorProps;
+
+class Pathname {
+  id: string;
+  value: string;
+  domainId: string;
+  trafficSourceId: string;
+  createdAt: Date;
+
+  private constructor(props: ConstructorProps) {
+    this.id = props.id;
+    this.value = props.value;
+    this.domainId = props.domainId;
+    this.trafficSourceId = props.trafficSourceId;
+    this.createdAt = props.createdAt;
+  }
+
+  static create(props: CreatePathnameProps) {
+    return new Pathname({
+      id: new IdAdapter().generate(),
+      value: props.value,
+      domainId: props.domainId,
+      trafficSourceId: props.trafficSourceId,
+      createdAt: new Date(),
+    });
+  }
+
+  static restore(props: RestorePathnameProps) {
+    return new Pathname({
+      id: props.id,
+      value: props.value,
+      domainId: props.domainId,
+      trafficSourceId: props.trafficSourceId,
+      createdAt: props.createdAt,
+    });
+  }
+
+  toJson() {
+    const formatDateAdapter = new FormatDateAdapter();
+    const createdAt = formatDateAdapter.format(this.createdAt);
+
+    return {
+      id: this.id,
+      value: this.value,
+      domainId: this.domainId,
+      trafficSourceId: this.trafficSourceId,
+      createdAt: createdAt,
+    };
+  }
+}
+
+export { Pathname };
